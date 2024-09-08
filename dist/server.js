375cc69d4194d29e7c736821f215f44009bc1eb3
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2024-09-08 12:27:01 +0530

    Resetting server.js

--- dist/server.js
+++ dist/server.js
@@ -9,10 +9,7 @@ class MainServer extends Server {
         super(true);
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: true }));
-        this.app.use(cors({
-            origin: "https://user-management-backend-sage.vercel.app/",
-            credentials: true,
-        }));
+        this.app.use(cors({ origin: '*' }));
         this.app.options('*', cors());
         this.setupControllers();
     }
@@ -20,10 +17,15 @@ class MainServer extends Server {
         const userController = new UserController();
         this.addControllers([userController]);
     }
+    start(port) {
+        this.app.listen(port, () => {
+            console.log(`Server is listening at ${port}`);
+        });
+    }
 }
 mongoose.connect(mongo_uri)
     .then(() => console.log('Connected to MongoDB database'))
     .catch(err => console.log(err));
 const server = new MainServer();
-const app = server.app;
-export default app;
+const PORT = 3000;
+server.start(PORT);
