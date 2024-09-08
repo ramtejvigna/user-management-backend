99583d9cb0b72c71c2693e990f97593e36468b5e
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2024-09-08 19:29:16 +0530

    Changes in Mongo URLs

--- dist/config.js
+++ dist/config.js
@@ -1,2 +1,2 @@
-export const mongo_uri = 'mongodb://localhost:27017/';
+export const mongo_uri = 'mongodb+srv://vignaramtejtelagarapu:vzNsqoKpAzHRdN9B@amile.auexv.mongodb.net/?retryWrites=true&w=majority&appName=Amile';
 export const jwt_secret = 'ramtej_vigna';
